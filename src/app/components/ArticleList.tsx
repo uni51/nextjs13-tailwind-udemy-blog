8884ff39328871122c5d6b3477145ea9763f2d85
uni51 <unieye51@gmail.com>
2024-01-11 17:19:32 +0900

    19. ブログ記事投稿一覧をHTMLとTailwindCSSで作ってみよう：その１

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -1,7 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const ArticleList = () => {
-  return <div>ArticleList</div>;
+  return (
+    <div>
+      <article>
+        <Link href="#">
+          <Image
+            src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
+            alt=""
+            width={1280}
+            height={300}
+          />
+        </Link>
+        <div>
+          <Link href="#">Technology</Link>
+          <Link href="#">Next.jsの勉強中</Link>
+          <p>By uni51, Published on 2024/01/11</p>
+          <Link href="#">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+            nostrum ea minus quisquam est voluptatum, saepe in. Animi beatae
+            odit explicabo cumque. Delectus, hic quae eaque dignissimos nesciunt
+            veniam accusantium?
+          </Link>
+          <Link href="#">続きを読む</Link>
+        </div>
+      </article>
+    </div>
+  );
 };
 
 export default ArticleList;
