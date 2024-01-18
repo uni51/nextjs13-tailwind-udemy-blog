4f9dda18513ac2fd72e85478c255ba4e56a84038
shimomura-iplug <koichi.shimomura@i-plug.co.jp>
2024-01-18 16:30:10 +0900

    26. エラー用ページを作成して意図的にエラーを発生させよう

--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -3,6 +3,10 @@ import { Article } from "./types";
 export const getAllArticles = async (): Promise<Article[]> => {
   const res = await fetch("http://localhost:3001/posts", { cache: "no-store" }); // SSR
 
+  if (!res.ok) {
+    throw new Error("Error from getAllArticles");
+  }
+
   const articles = await res.json();
   return articles;
 };
