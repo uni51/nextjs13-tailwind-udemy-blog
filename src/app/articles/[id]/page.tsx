630af4d319dc95c9d57af8ae47dc6b5d70a3287b
uni51 <unieye51@gmail.com>
2024-01-22 15:07:22 +0900

    30. 詳細記事内容の表示とサムネイル表示を修正しよう

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 import Image from "next/image";
+import { getDetailArticle } from "@/blogAPI";
 
-const Article = ({ params }: { params: { id: string } }) => {
-  console.log(params.id);
+const Article = async ({ params }: { params: { id: string } }) => {
+  const detailArticle = await getDetailArticle(params.id);
 
   return (
     <div className="max-w-3xl mx-auto p-5">
       {/* relative を追加 */}
       <Image
-        src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
+        src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${detailArticle.id}`}
         alt=""
         width={1280}
         height={300}
       />
-      <h1 className="text-4xl text-center mb-10 mt-10">ここがタイトルです。</h1>
+      <h1 className="text-4xl text-center mb-10 mt-10">
+        {detailArticle.title}
+      </h1>
       <div className="text-lg leading-relaxed text-justify">
-        <p>ここが本文です。</p>
+        <p>{detailArticle.content}</p>
       </div>
     </div>
   );
--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -1,14 +1,34 @@
+import { notFound } from "next/navigation";
 import { Article } from "./types";
 
 export const getAllArticles = async (): Promise<Article[]> => {
   const res = await fetch("http://localhost:3001/posts", { cache: "no-store" }); // SSR
 
   if (!res.ok) {
-    throw new Error("Error from getAllArticles");
+    throw new Error("エラーが発生しました。");
   }
 
-  await new Promise((resolve) => setTimeout(resolve, 1500)); // fake delay
+  await new Promise((resolve) => setTimeout(resolve, 1000)); // fake delay
 
   const articles = await res.json();
   return articles;
+};
+
+export const getDetailArticle = async (id: string): Promise<Article> => {
+  const res = await fetch(`http://localhost:3001/posts/${id}`, {
+    next: { revalidate: 60 },
+  }); // ISR
+
+  if (res.status === 404) {
+    notFound();
+  }
+
+  if (!res.ok) {
+    throw new Error("エラーが発生しました。");
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 1000)); // fake delay
+
+  const article = await res.json();
+  return article;
 };
